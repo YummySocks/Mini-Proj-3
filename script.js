3feabbaa36055aeda0c69c4d0250ccfc2ef20fcd
Socks <patricksutcliffe123@gmail.com>
2021-09-16 11:51:09 -0400

    rest of it

--- script.js
+++ script.js
@@ -2,10 +2,43 @@ var wins = 0;
 var losses = 0;
 var ties = 0;
 
-var options = ['R','P','S']
+var options = ["R","P","S"]
 
 var runGame = function(){
-    window.prompt("Select R, P, or S!");
+    var userSel = window.prompt("Select R, P, or S!");
+
+    if (!userSel){
+        return window.alert("you didn't even try!");
+    }
+
+    userSel = userSel.toUpperCase();
+
+    var randComp = Math.floor(Math.random() * options.length);
+    var compSel = options[randComp];
+
+    window.alert("the computer chose " + compSel);
+
+    if (userSel === compSel){
+        ties++;
+        window.alert("It's a tie!");
+    } else if ((userSel === "R" && compSel ==="S") || (userSel === "S" && compSel ==="P") || (userSel === "P" && compSel === "R")){
+        wins++;
+        window.alert("you win!");
+
+    } else{
+        losses++;
+        window.alert("you lose!");
+    };
+    
+    window.alert("Stats:\nWins: " + wins + "\nLosses: " + losses + "\nTies: " + ties);
+
+    var runAgain = window.confirm("would you like to go again?")
+    if (runAgain){
+        runGame();
+    } else{
+        window.alert("Scared potter?")
+    };
+
 
 }
 
